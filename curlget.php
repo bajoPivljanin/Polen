50535afca09844d9c8c9aed53526e1d864a13137
bajoPivljanin <103775541+bajoPivljanin@users.noreply.github.com>
2023-12-18 23:53:25 +0100

    edit nesto

--- curlget.php
+++ curlget.php
@@ -9,54 +9,6 @@ function fetch_cities(){
         return $decoded = json_decode($resp,true);
     }
 }
-/*function fetch_polen($city, $date_after, $date_before) {
-    $url_polen = "http://polen.sepa.gov.rs/api/opendata/pollens/?location=$city&date_after=$date_after&date_before=$date_before";
-    $resp = file_get_contents($url_polen);
-    if ($resp === false) {
-        return "Error fetching pollen data";
-    } else {
-        $decoded_polen = json_decode($resp, true);
-        if (isset($decoded_polen['results']) && is_array($decoded_polen['results'])) {
-            $allergensMap = [];
-
-            foreach ($decoded_polen['results'] as $item) {
-                $date = $item['date'];
-                $allergens = [];
-
-                if (isset($item['concentrations']) && is_array($item['concentrations'])) {
-                    foreach ($item['concentrations'] as $con) {
-                        $url_concentrations = "http://polen.sepa.gov.rs/api/opendata/concentrations/$con";
-                        $resp_concentrations = file_get_contents($url_concentrations);
-
-                        if ($resp_concentrations === false) {
-                            return "Error fetching concentration data";
-                        } else {
-                            $decoded_concentrations = json_decode($resp_concentrations, true);
-
-                            if (isset($decoded_concentrations['allergen'])) {
-                                $allergenId = $decoded_concentrations['allergen'];
-                                $url_allergen = "http://polen.sepa.gov.rs/api/opendata/allergens/$allergenId";
-                                $resp_allergen = file_get_contents($url_allergen);
-
-                                if ($resp_allergen === false) {
-                                    return "Error fetching allergen data";
-                                } else {
-                                    $decoded_allergen = json_decode($resp_allergen, true);
-                                    $allergens[] = $decoded_allergen;
-                                }
-                            }
-                        }
-                    }
-                }
-                $allergensMap[$date] = $allergens;
-            }
-            return $allergensMap;
-        }
-        else {
-            return "No results found";
-        }
-    }
-}*/
 function fetch_polen($city, $date_after, $date_before) {
     $url_polen = "http://polen.sepa.gov.rs/api/opendata/pollens/?location=$city&date_after=$date_after&date_before=$date_before";
     $resp = file_get_contents($url_polen);
